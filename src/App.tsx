074f7d0264badaf93dd9a113d2d5973eb238897b
ER1994 <94571379+ER1994@users.noreply.github.com>
2023-08-08 22:32:46 +0200

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import './App.css';
 import {Navbar} from "./components/Navbar";
 import {Home} from "./components/Home";
 import {Contact} from "./components/Contact";
+import {Footer} from "./components/Footer";
 
 function App() {
 
     return (
         <div className="App">
-            <header>
-            </header>
             <Navbar></Navbar>
+            <main>
             <Home></Home>
             <Contact></Contact>
+            </main>
+            <Footer></Footer>
         </div>
     );
 }
